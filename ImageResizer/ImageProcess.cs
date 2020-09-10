a46985f9fe9ae323b04b45258105b0128eb5a7d0
Wuhsien167 <Wuhsien167@gmail.com>
2020-09-10 20:30:55 +0800

    新增非同步方法

--- ImageResizer/ImageProcess.cs
+++ ImageResizer/ImageProcess.cs
@@ -3,7 +3,9 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
-
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
 namespace ImageResizer
 {
     public class ImageProcess
@@ -28,7 +30,40 @@ namespace ImageResizer
                 }
             }
         }
+        /// <summary>
+        /// 非同步進行圖片的縮放作業
+        /// </summary>
+        /// <param name="sourcePath">圖片來源目錄路徑</param>
+        /// <param name="destPath">產生圖片目的目錄路徑</param>
+        /// <param name="scale">縮放比例</param>
+        public async Task ResizeImagesAsync(string sourcePath, string destPath, double scale)
+        {
+            var allFiles = FindImages(sourcePath);
+            var taskbag = new ConcurrentBag<Task>();
+            foreach (var filePath in allFiles)
+            {
+                var t = Task.Run(() =>
+                {
+                    Image imgPhoto = Image.FromFile(filePath);
+                    string imgName = Path.GetFileNameWithoutExtension(filePath);
 
+                    int sourceWidth = imgPhoto.Width;
+                    int sourceHeight = imgPhoto.Height;
+
+                    int destionatonWidth = (int)(sourceWidth * scale);
+                    int destionatonHeight = (int)(sourceHeight * scale);
+
+                    Bitmap processedImage = processBitmap((Bitmap)imgPhoto,
+                        sourceWidth, sourceHeight,
+                        destionatonWidth, destionatonHeight);
+
+                    string destFile = Path.Combine(destPath, imgName + ".jpg");
+                    processedImage.Save(destFile, ImageFormat.Jpeg);
+                });
+                taskbag.Add(t);
+            }
+            await Task.WhenAll(taskbag);
+        }
         /// <summary>
         /// 進行圖片的縮放作業
         /// </summary>
--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -9,7 +9,7 @@ namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
             string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
@@ -20,10 +20,13 @@ namespace ImageResizer
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            //imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+
             sw.Stop();
 
             Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            Console.ReadKey();
         }
     }
 }
